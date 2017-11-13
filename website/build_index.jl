b3746b7419d47aa99c3ecf5ef45a674531072f93
Alex Arslan <ararslan@comcast.net>
2017-11-13 14:01:15 -0800

    Drop 0.4, add 0.7

--- website/build_index.jl
+++ website/build_index.jl
@@ -38,10 +38,10 @@ end
 pkg_names = sort(collect(keys(pkgs_by_name)))
 
 # Load logo
-jllogo_svg = readstring("html/jllogo.svg")
+jllogo_svg = read("html/jllogo.svg", String)
 
 # Load stylesheet
-pkg_css = readstring("html/pkg.css")
+pkg_css = read("html/pkg.css", String)
 
 # Load and render header
 index_head = Mustache.render(readstring("html/indexhead.html"),
@@ -51,13 +51,13 @@ index_head = Mustache.render(readstring("html/indexhead.html"),
          "PKGCOUNT"    => string(length(pkg_names))) )
 
 # Load footer (no templates used)
-index_foot = readstring("html/indexfoot.html")
+index_foot = read("html/indexfoot.html", String)
 
 # Load the template for a package
-index_pkg = readstring("html/indexpkg.html")
+index_pkg = read("html/indexpkg.html", String)
 
 # Load the package detail template
-pkg_detail = readstring("html/pkgdetail.html")
+pkg_detail = read("html/pkgdetail.html", String)
 
 # Helper function to produce history listings for each package
 function hist_table(hist_db, pkg_name, jl_ver)
@@ -100,7 +100,7 @@ end
 
 
 
-listings = UTF8String[]
+listings = String[]
 
 # For each package in the data set
 for pkg_name in pkg_names
